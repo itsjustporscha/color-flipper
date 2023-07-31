f5d13bd4063828690e511a7a35acaa6145bff733
Porscha <itsjustporscha@gmail.com>
2023-07-30 23:17:32 -0400

    "completed color flipper"

--- app.js
+++ app.js
@@ -2,9 +2,13 @@ const newColorBtnElement = document.getElementById("new-color-btn");
 
 const currentColorElement = document.getElementById("current-color");
 
+newColorBtnElement.addEventListener("click", function(){
+alert ('ok');
+});
+
 // 0,1,2,3,4,5,6,7,8,9,A,B,C,D,E,F Hexavalue
 
-const hexvalues = [
+const hexValues = [
   "0",
   "1",
   "2",
@@ -24,7 +28,7 @@ const hexvalues = [
 ];
 
 function getRandomHexValue() {
-  const randonIndexPosition = Math.floor(Math.random() * hexvalues.length);
+  const randomIndexPosition = Math.floor(Math.random() * hexValues.length);
 
   const randomHexValue = hexValues[randomIndexPosition];
 
@@ -41,6 +45,9 @@ function getRandomHexString(stringLength) {
 }
 
 newColorBtnElement.addEventListener("click", function () {
-  const randomHexString = getRandomHexString(6);
-  alert("randomHexString");
+  const randomHexString = "#" + getRandomHexString(6);
+
+  document.body.style.setProperty("background-color", randomHexString);
+
+  currentColorElement.textContent = randomHexString;
 });
